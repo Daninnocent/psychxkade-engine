93c800304e88168997e7ea61ba3354ffebaac0d9
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-04-29 23:46:20 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -83,6 +83,26 @@ class Main extends Sprite
 		
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 
+                var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
+
+                #if web
+                var str1:String = "HTML CRAP";
+                var vHandler = new VideoHandler();
+                vHandler.init1();
+                vHandler.video.name = str1;
+                addChild(vHandler.video);
+                vHandler.init2();
+                GlobalVideo.setVid(vHandler);
+                vHandler.source(ourSource);
+                #elseif sys 
+                var str1:String = "WEBM SHIT"; 
+                var webmHandle = new WebmHandler();
+                webmHandle.source(ourSource);
+                webmHandle.makePlayer();
+                webmHandle.webm.name = str1;
+                addChild(webmHandle.webm);
+                GlobalVideo.setWebm(webmHandle);
+                #end
 		
 		addChild(game);
 
