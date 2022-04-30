9aa446c735a55f96d0982df7bbb513e8384be80a
Erickoxd <93102304+Unwiel@users.noreply.github.com>
2022-04-30 10:52:09 -0500

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -106,12 +106,9 @@ class Main extends Sprite
 		
 		addChild(game);
 
-		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
-
-		#end
 	}
 
 	var game:FlxGame;
